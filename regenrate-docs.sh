9557fa64c6d78b901936ac12d11dd7fb3ee4093e
Tor Håvard Bieltvedt <th@bieltvedt.net>
2017-10-08 18:47:18 +0200

    New version regenerate-docs.sh

--- regenrate-docs.sh
+++ regenrate-docs.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=`dirname $0`
-DOCS_DIR=${SCRIPT_DIR}/docs
-DIST_DIR=${SCRIPT_DIR}/dist
-
-rm -r ${DOCS_DIR}/**
-cp -a ${DIST_DIR}/. ${DOCS_DIR}/
-
-echo 'Files copied from generated ../dist folder.' > ${DOCS_DIR}/README.txt
+ng build --output-path docs
